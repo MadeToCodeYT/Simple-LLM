37c2e92939f23b9994edf6713a93c09d4efbc0bf
madetocodeyt <madetocodeyt@gmail.com>
2026-09-07 17:42:51 -0400

    Calculated attention scores
    Also scaled them by sqrt(32)

--- attention.py
+++ attention.py
@@ -24,12 +24,13 @@ def calc_prod(vector: list[float], matrix: list[list[float]]) -> list[float]:
 
 EMBEDDING_SIZE = 32
 ATTENTION_SIZE = 32
+ATTENTION_SQRT = ATTENTION_SIZE**0.5
 
 W_Q = [generate_random_vector(EMBEDDING_SIZE) for _ in range(EMBEDDING_SIZE)]
 W_K = [generate_random_vector(EMBEDDING_SIZE) for _ in range(EMBEDDING_SIZE)]
 W_V = [generate_random_vector(EMBEDDING_SIZE) for _ in range(EMBEDDING_SIZE)]
 
-def get_qkv(embeddings: list[list[float]]) -> dict[str, list]:
+def get_qkv(embeddings: list[list[float]]) -> dict[str, list[list[float]]]:
     queries = []
     keys = []
     values = []
@@ -44,3 +45,25 @@ def get_qkv(embeddings: list[list[float]]) -> dict[str, list]:
         "keys": keys,
         "values": values,
     }
+
+def attention_score(query: list[float], key: list[float]) -> float:
+    score = 0
+
+    for i in range(len(query)):
+        score += query[i]*key[i]
+
+    return score
+
+def calculate_scores(queries: list[list[float]], keys: list[list[float]]):
+    score_table = []
+
+    for query in queries:
+        row = []
+
+        for key in keys:
+            score = attention_score(query, key) / ATTENTION_SQRT
+            row.append(score)
+
+        score_table.append(row)
+
+    return score_table
